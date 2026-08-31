087ca144bcdbae370e415068c490b8f9206d06e0
Deniz Kavzak <deniz.kavzak@gmail.com>
2026-08-31 15:10:03 -0500

    Simple example of a private constructor use
    MathUtils class with a private constructor and with a public static method
    that can be used without the need of an object. Utility classes like this
    example are a good use case for private constructors.

---
+++ week1/PrivateConstructorExampleCalculator/src/edu/emu/math/utils/MathUtils.java
@@ -0,0 +1,11 @@
+package edu.emu.math.utils;
+
+public final class MathUtils {
+    private MathUtils() {
+    	// Prevent instances of this class
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ week1/PrivateConstructorExampleCalculator/src/edu/mu/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package edu.mu.calculator;
+
+import edu.emu.math.utils.MathUtils;
+
+public class Calculator {
+	
+    public static void main(String[] args) {
+        int result = MathUtils.add(1, 2);
+
+        System.out.println(result); // 3
+    }
+
+}
