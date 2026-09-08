2891028fdaf7ceb477bb074bcfcc3a726d69fdee
Deniz Kavzak <deniz.kavzak@gmail.com>
2026-09-08 15:59:58 -0500

    V2 applies basic encapsulation principles to V1
    public instance variables become private. Get and set methods are
    implemented for these private instance variables. Get and set
    methods are named directly as setX, getX, where X is the instance
    variable. Main method now uses those methods from BankAccount class.

---
+++ week2/Week2DesignExample/src/edu/mu/design/bank/v2/BankAccount.java
@@ -0,0 +1,23 @@
+package edu.mu.design.bank.v2;
+
+public class BankAccount {
+	private String owner; 
+	private double balance;
+	
+	public String getOwner() {
+		return owner;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+}
