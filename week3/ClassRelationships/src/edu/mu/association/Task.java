7d02843a1b2e12209eba7ec4f995e30e2b7a0dcc
Deniz Kavzak <deniz.kavzak@gmail.com>
2026-09-11 16:10:02 -0500

    Association relationship example

---
+++ week3/ClassRelationships/src/edu/mu/association/Task.java
@@ -0,0 +1,7 @@
+package edu.mu.association;
+
+public class Task {
+	private int taskID;
+	private String description;
+	private User assignedTo;
+}
---
+++ week3/ClassRelationships/src/edu/mu/association/User.java
@@ -0,0 +1,8 @@
+package edu.mu.association;
+
+import java.util.List;
+
+public class User {
+	private int userID;
+	private List<Task> tasks;
+}
